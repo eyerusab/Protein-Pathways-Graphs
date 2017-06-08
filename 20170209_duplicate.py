e43d099796fa34e807e319f79e20796eca3459e5
eyerusab <ea2287a@student.american.edu>
2017-06-08 15:16:15 -0400

    Create 20170209_duplicate.py

---
+++ 20170209_duplicate.py
@@ -0,0 +1,22 @@
+#This file allows for the organization and reading of the pathways dataset
+
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+with open('SelectedPathways.csv', 'rb') as f:
+	ncols = len(next(f).split(','))
+	
+x=np.genfromtxt('SelectedPathways.csv', delimiter=',', dtype=None, names=None,
+		usecols=range(0,ncols)
+		)
+
+network =[]
+for row in x:
+	newrow=[]
+	for col in row:
+		if col!='':
+			newrow.append(col)
+	network.append(newrow)
+	
